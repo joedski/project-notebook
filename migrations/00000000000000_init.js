bf93adbdcb5b79c54832e5df6ffffc67ee37307a
Joseph Sikorski <joedski@gmail.com>
2018-12-23 22:55:54 -0500

    Initial migration: entity, page

--- migrations/00000000000000_init.js
+++ migrations/00000000000000_init.js
@@ -1,8 +1,27 @@
 
 exports.up = async function (knex, Promise) {
-  // TODO!
+  await knex.schema.createTable('entity', table => {
+    table.increments('id')
+    .comment('Entity Id')
+  })
+
+  await knex.schema.createTable('page', table => {
+    table.integer('id').unsigned().notNullable()
+    .comment('Page Id (Entity Id)')
+    table.foreign('id').references('id').inTable('entity')
+
+    table.text('content')
+    .comment('Page Content, Serialized; may not be text search friendly')
+
+    table.text('summary')
+    .comment('Short summary of the contents of this page, conclusions, experiment results, etc')
+
+    table.timestamp('created_at').defaultTo(knex.fn.now())
+    .comment('When this Page was created')
+  })
 };
 
 exports.down = async function (knex, Promise) {
-  // TODO!
+  await knex.schema.dropTable('page')
+  await knex.schema.dropTable('entity')
 };
